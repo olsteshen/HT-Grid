aa3c1062fe1c3db62c723297848113e6c03a4a2a
stesho01 <olga_steshenko@epam.com>
2022-08-01 23:35:54 +0200

    Cross browser testing using parameter

--- src/main/java/driver/SingletonDriver.java
+++ src/main/java/driver/SingletonDriver.java
@@ -3,6 +3,7 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -15,12 +16,21 @@ public class SingletonDriver {
     private static final int IMPLICIT_WAIT_TIMEOUT = 5;
     private static final int PAGE_LOAD_TIMEOUT = 10;
     private final static String CHROME_DRIVER_PATH = "./src/main/resources/chromedriver";
+    private final static String FIREFOX_DRIVER_PATH = "./src/main/resources/geckodriver";
     private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
 
     private SingletonDriver() {
-        System.setProperty(CHROME_DRIVER_EXE_PROPERTY, CHROME_DRIVER_PATH);
-        WebDriverManager.chromedriver().driverVersion("103.0.5060.114").setup();
-        driver = new ChromeDriver();
+        String browser = System.getProperty("browser");
+                if(browser.equalsIgnoreCase("chrome")){
+            System.setProperty(CHROME_DRIVER_EXE_PROPERTY, CHROME_DRIVER_PATH);
+            WebDriverManager.chromedriver().driverVersion("103.0.5060.114").setup();
+            driver = new ChromeDriver();
+        }
+                if(browser.equalsIgnoreCase("firefox")){
+                    System.setProperty("webdriver.gecko.driver", FIREFOX_DRIVER_PATH);
+                    driver = new FirefoxDriver();
+                }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT_TIMEOUT));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGE_LOAD_TIMEOUT));
